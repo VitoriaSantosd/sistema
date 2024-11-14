d9df3c5b033eb4cc1fb7918b706d9b97c96004e0
Vitória Santosd <santos.eduarda.f.s@gmail.com>
2024-11-13 23:42:13 -0300

    bd

--- calendario_academico/settings.py
+++ calendario_academico/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'calendario_academico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.hszydxwkkomvpcezlzus',
+        'PASSWORD': 'sua_senha',
+        'HOST': 'aws-0-sa-east-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
